ffd1d85a208e5fa6a15a7a768c0e59e4024cd04b
Cathal Kavanagh <x19182121@student.ncirl.ie>
2020-03-31 18:21:57 +0100

    add account and transaction models

---
+++ src/main/java/apis/CA3/Models/Account.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apis.CA3.Models;
+
+import java.util.List;
+
+/**
+ *
+ * @author x19182121
+ */
+public class Account {
+  private int id;
+    private int customerID;
+    private String sortCode;
+    private String number;
+    private double balance;
+    private List<Transaction> transactions;
+    private String title;
+    
+    public Account() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getSortCode() {
+        return sortCode;
+    }
+
+    public void setSortCode(String sortCode) {
+        this.sortCode = sortCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setAccountNumber(String number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAccountType(String title) {
+        this.title = title;
+    }
+       
+}
---
+++ src/main/java/apis/CA3/Models/Transaction.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apis.CA3.Models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author x19182121
+ */
+public class Transaction {
+  private int id;
+    private int accountID;
+    private char type;
+    private Date created;
+    private double amount;
+    private double postBalance;
+    
+    public Transaction() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getPostBalance() {
+        return postBalance;
+    }
+
+    public void setPostBalance(double postBalance) {
+        this.postBalance = postBalance;
+    }
+      
+}
